d1291135f57ade09de89333006d939807bb04cce
etchx <etchxs@gmail.com>
2023-07-16 11:26:47 -0400

    init

---
+++ src/main/java/io/github/etchx/entityrange/EntityRange.java
@@ -0,0 +1,13 @@
+package io.github.etchx.entityrange;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EntityRange implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
