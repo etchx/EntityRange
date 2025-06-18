73b4823675c6f407b95895f89fd498f69eb9b223
etchx <etchxs@gmail.com>
2025-06-17 23:48:24 -0400

    Added distance indicator fadeout

--- src/main/java/io/github/etchx/entityrange/mixin/InGameHudMixin.java
+++ src/main/java/io/github/etchx/entityrange/mixin/InGameHudMixin.java
@@ -4,12 +4,17 @@ import io.github.etchx.entityrange.client.EntityRangeClient;
 import io.github.etchx.entityrange.client.EntityRangeUtil;
 import io.github.etchx.entityrange.data.HitlogIO;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -29,6 +34,11 @@ import static io.github.etchx.entityrange.client.EntityRangeClient.hitData;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
+    @Shadow public abstract TextRenderer getTextRenderer();
+
+    @Unique
+    private int distanceDisplayFade = 0;
+
     /**
      * Renders EntityRange's hit and distance displays
      */
@@ -55,17 +65,33 @@ public abstract class InGameHudMixin {
         }
 
         // distance display
-        if (isTargetingEntity) {
-            isTargetingEntity = false;
-            if (!hideDistanceDisplay) {
+        if (!hideDistanceDisplay) {
+            MutableText text = Text.translatable(String.format("%.3f", raycastHitDistance));
+            int width = this.getTextRenderer().getWidth(text);
+            if (isTargetingEntity) {
                 int color = isInRange ? 0xFF0000 : 0xFFFFFF; // hardcoded colors
-                context.drawText(((InGameHud) (Object) this).getTextRenderer(),
-                        Text.translatable(String.format("%.3f", raycastHitDistance)),
-                        (context.getScaledWindowWidth() - 26) / 2,
+                context.drawText(this.getTextRenderer(),
+                        text,
+                        (context.getScaledWindowWidth() - width) / 2,
                         (context.getScaledWindowHeight() - 7) / 2 + 15,
                         color, true);
+                distanceDisplayFade = (int)(20.0 * MinecraftClient.getInstance().options.getNotificationDisplayTime().getValue());
+            } else if (distanceDisplayFade > 0) {
+                int alpha = distanceDisplayFade * 256 / 10;
+                if (alpha > 255) {
+                    alpha = 255;
+                }
+                if (alpha > 0) {
+                    context.drawText(this.getTextRenderer(),
+                            text,
+                            (context.getScaledWindowWidth() - width) / 2,
+                            (context.getScaledWindowHeight() - 7) / 2 + 15,
+                            ColorHelper.Argb.withAlpha(alpha, 0x555555), true);
+                }
+                distanceDisplayFade--;
             }
         }
+        isTargetingEntity = false;
 
         // side hit display
         if (!showHitsInChat && !hideHitDisplay) {
